3d59eefd05936b3c8506f7ede5a6ba8157c29a02
borissmetanin <boris.smetanin1703@gmail.com>
2019-02-22 16:39:01 +0200

    brunch-1 finished creating the basic architecture

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-console.log('hello');
-console.log('test');
+// List of TODO's
+// Include server file
+
+// Server will know if this is http/https requested will use the right functions for each request with a unified callback
+
+// unified server callback:
+// check if controller exists
+// check if method exists: (get/post/put/delete + (action name + id) / collection - like we do in kohana)
+// if controller not exists or if method does not exist in controller - callback 404 method
+
+
+/**
+ * Load dependencies
+ */
+
+let server = require('./lib/server');
+
+
+/**
+ * Container for the app
+ */
+let app = {};
+
+app.init = () => {
+    // Initialize the server
+    server.serve();
+}
+
+// Start the application
+app.init();
+
